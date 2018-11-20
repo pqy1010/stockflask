ded8960516a451c829429c3a98847100503e9db0
peng_t <pqy1010@sina.com>
2018-11-20 22:22:36 +0800

    stock state change is ok

--- stockapp/view.py
+++ stockapp/view.py
@@ -3,6 +3,7 @@ from .stockforms import LoginForm
 from flask_login import login_required
 from flask_login import login_user,logout_user
 from .models import User,StockState
+from . import db
 view=Blueprint('view',__name__)
 import sqlite3
 
@@ -70,13 +71,44 @@ def searchstock():
     elif int(reqval['cmd'])==3:
         pagination = StockState.query.filter_by(state='own').paginate(int(reqval['page']), per_page=8, error_out=False)
     elif int(reqval['cmd']) == 4:
-        pagination = StockState.query.filter_by(state='wait sell').paginate(int(reqval['page']), per_page=8, error_out=False)
+        pagination = StockState.query.filter_by(state='selling').paginate(int(reqval['page']), per_page=8, error_out=False)
     elif int(reqval['cmd']) == 5:
-        pagination = StockState.query.filter_by(state='sell').paginate(int(reqval['page']), per_page=8, error_out=False)
+        pagination = StockState.query.filter_by(state='sold').paginate(int(reqval['page']), per_page=8, error_out=False)
+    elif int(reqval['cmd']) == 6:
+        pagination = StockState.query.filter_by(state='buying').paginate(int(reqval['page']), per_page=8, error_out=False)
+    elif int(reqval['cmd']) == 7:
+        pagination = StockState.query.filter_by(state='ignore').paginate(int(reqval['page']), per_page=8,error_out=False)
 
     stocklist=pagination.items
     data={}
     data['stocklist']=stocklist
     data['displaylist'] = ['name', 'code', 'ctime', 'b_time', 'b_price', 'b_money', 'b_count', 'owntime', 'state', 'earn','earnrate', 's_price','s_time']
     data['cmd']=reqval['cmd']
+    data['page']=reqval['page']
     return render_template('stockdetail.html',data=data,pagination=pagination)
+
+@view.route('/updatestockstate')
+@login_required
+def updatestockstate():
+    reqval=request.values.to_dict()
+    stock=StockState.query.filter_by(ID=int(reqval['stockID'])).first()
+
+
+
+
+    upstate=int(reqval['upstate'])
+    if upstate==11:# wait buy->buying
+        stock.state='buying'
+    elif upstate==12:# wait buy->ignore
+        stock.state='ignore'
+    elif upstate==13:# buying->wait buy
+        stock.state='wait buy'
+    elif upstate==14:# own->selling
+        stock.state='selling'
+    elif upstate==15:# selling->own
+        stock.state='own'
+    elif upstate==16:# ignore->wait buy
+        stock.state='wait buy'
+    db.session.commit()
+
+    return redirect(url_for('view.searchstock',cmd=reqval['cmd'],page=reqval['page']))
